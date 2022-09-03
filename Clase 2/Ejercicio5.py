e16ac5ef7dfade46843f9cbd867d1a00a213dc91
Luciano Bardallo <lucianobardalloo@gmail.com>
2022-09-03 16:17:41 -0300

    Ejercicio 5 sin completar

---
+++ Clase 2/Ejercicio5.py
@@ -0,0 +1,41 @@
+from ast import Lambda
+
+
+habilidades = [
+    {
+        "Nombre": "Vision-X",
+        "Poder": 64
+    },
+    {
+        "Nombre": "Vuelo",
+        "Poder": 32
+    },
+    {
+        "Nombre": "Inteligencia",
+        "Poder": 256
+    },
+    {
+        "Nombre": "Metamorfosis",
+        "Poder": 1024
+    },
+    {
+        "Nombre": "Super Velocidad",
+        "Poder": 128
+    },
+    {
+        "Nombre": "Magia",
+        "Poder": 512
+    }
+]
+
+habilidades_UTN = []
+for habilidad in habilidades:
+    habilidades_selec = {}
+    habilidades_selec["Nombre"] = habilidad["Nombre"]
+    habilidades_selec["Poder"] = int(habilidad["Poder"])
+    habilidades_UTN.append(habilidades_selec)
+
+
+for poder in habilidades_UTN:
+    print(poder)
+
