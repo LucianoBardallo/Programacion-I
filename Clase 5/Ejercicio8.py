862e25b3ac14221ecb5033e5aafc2494f961bfa1
Luciano Bardallo <lucianobardalloo@gmail.com>
2022-09-17 11:18:49 -0300

    Ejercicio 8 TERMINADO

--- Clase 5/Ejercicio8.py
+++ Clase 5/Ejercicio8.py
@@ -1,35 +1,5 @@
 from data_stark import lista_personajes
 
-'''
-lista_heroes =
-[
- {
-   "nombre": "Howard the Duck",
-   "identidad": "Howard (Last name unrevealed)",
-   "empresa": "Marvel Comics",
-   "altura": "79.349999999999994",
-   "peso": "18.449999999999999",
-   "genero": "M",
-   "color_ojos": "Brown",
-   "color_pelo": "Yellow",
-   "fuerza": "2",
-   "inteligencia": "average"
- },
- {
-   "nombre": "Rocket Raccoon",
-   "identidad": "Rocket Raccoon",
-   "empresa": "Marvel Comics",
-   "altura": "122.77",
-   "peso": "25.73",
-   "genero": "M",
-   "color_ojos": "Brown",
-   "color_pelo": "Brown",
-   "fuerza": "5",
-   "inteligencia": "average"
- }
-]
-
-'''
 #---------------------PUNTO 0----------------
 def stark_normalizar_datos(lista:list):
     cambio = False
